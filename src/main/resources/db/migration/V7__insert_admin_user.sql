5e9c6c87040e9d9275237309894c4d65000153f7
Jackson <jacksonwendel@gmail.com>
2025-05-14 19:49:59 -0300

    Atualiza migração SQL para inserção de usuário administrador
    - Modifica a inserção do usuário administrador para garantir que ele seja criado apenas se não existir, utilizando uma estrutura de controle PL/pgSQL.
    - Atualiza os campos da tabela de usuários para refletir as mudanças na estrutura do banco de dados.

--- src/main/resources/db/migration/V7__insert_admin_user.sql
+++ src/main/resources/db/migration/V7__insert_admin_user.sql
@@ -1,32 +1,41 @@
--- Inserir usuário administrador inicial
-INSERT INTO usuario (
-    razao_social,
-    nome_fantasia,
-    cnpj,
-    endereco,
-    cidade,
-    uf,
-    cep,
-    telefone,
-    email,
-    senha,
-    tipo_usuario,
-    status,
-    data_cadastro,
-    ultima_atualizacao
-) VALUES (
-    'REV3RSO Admin',
-    'REV3RSO Admin',
-    '00000000000000',
-    'Endereço Administrativo',
-    'São Paulo',
-    'SP',
-    '00000000',
-    '(11) 0000-0000',
-    'admin@rev3rso.com.br',
-    '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', -- senha: admin
-    'ADMINISTRADOR',
-    'ATIVO',
-    CURRENT_TIMESTAMP,
-    CURRENT_TIMESTAMP
-); 
+-- Inserir usuário administrador inicial apenas se não existir
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM usuarios WHERE email = 'admin@rev3rso.com.br') THEN
+        INSERT INTO usuarios (
+            id,
+            nome,
+            razao_social,
+            nome_fantasia,
+            cnpj,
+            endereco,
+            cidade,
+            uf,
+            cep,
+            telefone,
+            email,
+            senha,
+            tipo_usuario,
+            status,
+            data_cadastro,
+            data_atualizacao
+        ) VALUES (
+            nextval('usuarios_seq'),
+            'REV3RSO Admin',
+            'REV3RSO Admin',
+            'REV3RSO Admin',
+            '00000000000000',
+            'Endereço Administrativo',
+            'São Paulo',
+            'SP',
+            '00000000',
+            '(11) 0000-0000',
+            'admin@rev3rso.com.br',
+            '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', -- senha: admin
+            'ADMINISTRADOR',
+            'ATIVO',
+            CURRENT_TIMESTAMP,
+            CURRENT_TIMESTAMP
+        );
+    END IF;
+END $$; 
